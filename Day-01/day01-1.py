062f267b21b73d1a2256683790fa59af7cfbf153
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-01 11:01:17 +0000

    Day 1 Part 1 Completed

--- Day-01/day01-1.py
+++ Day-01/day01-1.py
@@ -1,5 +1,21 @@
-def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+def main(raw_input):
+    data = parse_input(raw_input)
 
-    return str(solution)
+    total_fuel = 0
+    for module_mass in data:
+        module_fuel = (int(module_mass) // 3) - 2
+        total_fuel += module_fuel
+
+    return str(total_fuel)
+        
+
+def get_input(filename):
+    with open(filename) as f:
+        puzzle_input = f.read()
+    return puzzle_input
+
+def parse_input(raw_input):
+    return raw_input.split('\n')
+
+raw_input = get_input('input.txt')
+print(main(raw_input))
