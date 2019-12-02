a271b51a01d68ccab4cd36e4ac542619e027a11f
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-02 22:35:16 +0000

    Updated Templates

--- Day-22/day22-2.py
+++ Day-22/day22-2.py
@@ -1,5 +1,22 @@
-def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+def main(raw_input):
+    # Parse input
 
-    return str(solution)
+    # Solve problem
+
+    # Return solution
+    return None
+
+
+def get_input(filename):
+    with open(filename) as f:
+        raw_input = f.read()
+    return raw_input
+
+
+def parse_input(raw_input):
+    return raw_input
+
+
+if __name__ == '__main__':
+    puzzle_input = get_input('input.txt')
+    print(main(puzzle_input))
