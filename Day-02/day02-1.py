5adef07f5905589a47c381147cb46fbb6701822a
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-02 13:13:31 +0000

    started day 2

--- Day-02/day02-1.py
+++ Day-02/day02-1.py
@@ -1,5 +1,32 @@
-def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+def main(raw_input):
+    data = [int(x) for x in parse_input(raw_input)]
 
-    return str(solution)
+    # Add final config
+    data[1] = 12
+    data[2] = 2
+
+    # Run program
+    pos = 0
+    while data[pos] != 99:
+        if data[pos] == 1:
+            data[data[pos + 3]] = data[data[pos + 1]] + data[data[pos + 2]]
+        elif data[pos] == 2:
+            data[data[pos + 3]] = data[data[pos + 1]] * data[data[pos + 2]]
+        pos += 4
+
+    return str(data[0])
+
+
+def get_input(filename):
+    with open(filename) as f:
+        raw_input = f.read()
+    return raw_input
+
+
+def parse_input(raw_input):
+    return raw_input.split(',')
+
+
+if __name__ == '__main__':
+    puzzle_input = get_input('test1.txt')
+    print(main(puzzle_input))
