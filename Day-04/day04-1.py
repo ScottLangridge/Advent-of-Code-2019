ef727d64519891f7dedae3ba0183d880d318387f
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-07 23:50:13 +0000

    Quick and dirty solution to 4-1

--- Day-04/day04-1.py
+++ Day-04/day04-1.py
@@ -1,10 +1,32 @@
 def main(raw_input):
-    # Parse input
+    lower, upper = parse_input(raw_input)
 
-    # Solve problem
+    count = 0
+    for i in range(lower, upper + 1):
+        if is_valid(i):
+            count += 1
 
-    # Return solution
-    return None
+    return count
+
+
+def is_valid(num):
+    return never_decreases(num) and has_matching_adjacents(num)
+
+
+def never_decreases(num):
+    num = str(num)
+    for i in range(len(num) - 1):
+        if int(num[i]) > int(num[i + 1]):
+            return False
+    return True
+
+
+def has_matching_adjacents(num):
+    num = str(num)
+    for i in range(len(num) - 1):
+        if num[i] == num[i + 1]:
+            return True
+    return False
 
 
 def get_input(filename):
@@ -14,7 +36,7 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    return [int(i) for i in raw_input.split('-')]
 
 
 if __name__ == '__main__':
