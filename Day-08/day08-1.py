5516e2c38da54092134ae8ad34e5082dcccca8cd
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-08 12:06:03 +0000

    Solved Day8 part 1

--- Day-08/day08-1.py
+++ Day-08/day08-1.py
@@ -1,10 +1,32 @@
 def main(raw_input):
-    # Parse input
+    data = [int(val) for val in parse_input(raw_input)]
+    dimensions = (25, 6)
+    pixel_count = dimensions[0] * dimensions[1]
 
-    # Solve problem
+    # Build layers
+    layers = []
+    input_pointer = 0
+    while input_pointer < len(data):
+        current_layer = []
+        for _ in range(pixel_count):
+            current_layer.append(data[input_pointer])
+            input_pointer += 1
+        layers.append(current_layer)
 
-    # Return solution
-    return None
+    # Find layer with least 0s
+    min_zero_layer = layers[0]
+    min_zero_count = layers[0].count(0)
+    for layer in layers[1:]:
+        if layer.count(0) < min_zero_count:
+            min_zero_layer = layer
+            min_zero_count = layer.count(0)
+
+    # Generate output
+    ones = min_zero_layer.count(1)
+    twos = min_zero_layer.count(2)
+    val = ones * twos
+
+    return val
 
 
 def get_input(filename):
@@ -14,7 +36,7 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    return list(raw_input)
 
 
 if __name__ == '__main__':
