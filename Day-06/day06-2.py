623f0cf9df7ec80f8bea660a2a548475354fe313
Scott Langridge <scott.m.langridge@gmail.com>
2019-12-08 02:01:59 +0000

    Solved Day6 part 2

--- Day-06/day06-2.py
+++ Day-06/day06-2.py
@@ -1,10 +1,39 @@
+from collections import defaultdict
+
+
 def main(raw_input):
-    # Parse input
+    data = parse_input(raw_input)
 
-    # Solve problem
+    # Map orbits
+    parent = {}
+    for (v, k) in data:
+        parent[k] = v
 
-    # Return solution
-    return None
+    # Find first common node with santa
+    current = parent['YOU']
+    you_path = []
+    while current != 'COM':
+        you_path.append(current)
+        current = parent[current]
+
+    current = parent['SAN']
+    san_path = []
+    while current != 'COM':
+        san_path.append(current)
+        current = parent[current]
+
+    for i in you_path:
+        if i in san_path:
+            return you_path.index(i) + san_path.index(i)
+
+
+def lst_children(orbits, k):
+    children = orbits[k]
+    for i in orbits[k]:
+        for child in lst_children(orbits, i):
+            if child not in children:
+                children.append(child)
+    return children
 
 
 def get_input(filename):
@@ -14,7 +43,7 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    return [line.split(')') for line in raw_input.splitlines()]
 
 
 if __name__ == '__main__':
